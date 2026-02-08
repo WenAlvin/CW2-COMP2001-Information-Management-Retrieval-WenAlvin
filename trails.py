b1aa13e54f67d129432d43b8cb5d74bbd472c592
WenAlvin <tsz.wen@students.plymouth.ac.uk>
2026-02-08 19:33:54 +0800

    Update swagger.yml to reflect new endpoints and request/response formats.

--- trails.py
+++ trails.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import abort
 
 
 def get_timestamp():
@@ -30,3 +31,29 @@ TRAILS = {
 }
 def read_all():
     return list(TRAILS.values())
+def create(trail):
+    if "name" not in trail:
+        TRAILS[trail["name"]] = trail
+        trail["CreatedDate"] = get_timestamp()
+        return trail, 201
+    else:
+        abort(400, f"Trail with name {trail['name']} already exists")
+def read_one(trail_id):
+    if trail_id in TRAILS:
+        return TRAILS[trail_id]
+    else:
+        abort(404, f"Trail with id {trail_id} not found")
+def update(trail_id, trail):
+    if trail_id in TRAILS:
+        trail["id"] = trail_id
+        trail["CreatedDate"] = get_timestamp()
+        TRAILS[trail_id] = trail
+        return trail
+    else:
+        abort(404, f"Trail with id {trail_id} not found")
+def delete(trail_id):
+    if trail_id in TRAILS:
+        del TRAILS[trail_id]
+        return f"Trail {trail_id} successfully deleted", 204
+    else:
+        abort(404, f"Trail with id {trail_id} not found")
